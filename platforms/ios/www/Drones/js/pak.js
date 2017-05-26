499a6538716a87afef4f8d3a811f154dccd5d637
h205p2 <edmondtu@students.berkeley.net>
2017-05-26 12:02:07 -0700

    First commit

---
+++ platforms/ios/www/Drones/js/pak.js
@@ -0,0 +1,45 @@
+/**
+ * Created by h205p2 on 5/24/17.
+ */
+var pakistan = [];
+
+
+$(document).ready(function(){
+    $.ajax({
+        url: 'http://api.dronestre.am/data',
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function (result) {
+            console.log(result);
+            pak(result)
+        },
+        error: function () {
+            alert('Failed!');
+        }
+    });
+});
+
+
+function pak(result){
+    for(var i = 0; i<result.strike.length; i++){
+        if(result.strike[i].country==="Pakistan"){
+            pakistan.push(result.strike[i])
+        }
+    }
+}
+
+console.log(pakistan);
+
+
+function myMap() {
+    var mapProp = {
+        center:new google.maps.LatLng(30.3753, 69.3451),
+        zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+function markers() {
+
+}
